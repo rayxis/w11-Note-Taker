49e268cbfe98aa3cc59b9ef8cb8a95ffa7cab014
Ray <ray@rayxis.com>
2024-02-15 10:25:01 -0600

    Fixed the port issue for heroku deployment.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path    = require('path');
 const app     = express();
 
 // Port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Setup Express to handle JSON, encoded URL data, and serve static pages.
 app.use(express.json());
