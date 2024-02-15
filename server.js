ac2f909f48b31fd30569fef628da37383a98c463
Ray <ray@rayxis.com>
2024-02-15 05:51:28 -0600

    Added handling for notes.html, and db.json entries.

--- server.js
+++ server.js
@@ -13,7 +13,26 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // Serve index.html
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+
+   // Serve notes
+   .get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')))
+
+	// Get the notes from the database JSON file, and return them.
+   .get('/api/notes', (req, res) => {
+	   console.log('GET /api/notes');
+	   // Read the file and return a JSON string to the user.
+	   fs.readFile('./db/db.json').then(data => res.json(noteData));
+   })
+
+	// Get a specific note based off of the ID.
+   .get('/api/notes/:id', (req, res) => {
+	   fs.readFile('./db/db.json').then(data => {
+		   console.log('GET /api/notes/' + req.params.id);
+		   // Read the file and return a JSON string to the user.
+		   res.json(JSON.parse(data)[req.params.id]);
+	   });
+   });
 
 // Open the port and listen
 app.listen(PORT, () => console.log(`Server is running at https://localhost:${PORT}`));
